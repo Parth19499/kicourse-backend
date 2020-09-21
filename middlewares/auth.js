9d05b02e431859578717d8088dd674df851b962d
Parth Panchal <parth19499@gmail.com>
2020-09-22 00:24:17 +0530

    authentication bugs fixed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,10 +6,7 @@ exports.auth = (req, res, next) => {
   const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Access denied! No token provided.");
   try {
-    // winston.info(`token: ${token}`);
     const decoded = jwt.verify(token, config.get("app.jwtPrivateKey"));
-    let j = JSON.stringify(decoded);
-    // winston.info(`decoded: ${j}`);
     req.user = decoded;
     next();
   } catch (ex) {
